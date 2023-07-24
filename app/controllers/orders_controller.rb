ebc72a32182f088df29d8e86f95873791b672740
Finola Moloney <x22151338@student.ncirl.ie>
2023-07-24 22:48:32 +0100

    fixed item table

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -24,17 +24,37 @@ class OrdersController < ApplicationController
   # POST /orders or /orders.json
   def create
     @order = Order.new(order_params)
-
+    puts "Received order params: #{order_params.inspect}"
+    
     respond_to do |format|
       if @order.save
+        # Save the order first to assign an ID
         format.html { redirect_to order_url(@order), notice: "Order was successfully created." }
         format.json { render :show, status: :created, location: @order }
+  
+        # Create order items for the new order
+        create_order_items_for_order(@order)
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @order.errors, status: :unprocessable_entity }
       end
     end
   end
+  
+  def create_order_items_for_order(order)
+    puts "Creating order items..."
+    params[:order_items].each do |item_params|
+      puts "Item params: #{item_params.inspect}"
+  
+      order_item = OrderItem.create(
+        order_id: order.id,
+        title: item_params[:title],
+        description: item_params[:description],
+        price: item_params[:price],
+        quantity: item_params[:quantity]
+      )
+    end
+  end 
 
   # PATCH/PUT /orders/1 or /orders/1.json
   def update
@@ -67,6 +87,14 @@ class OrdersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def order_params
-      params.require(:order).permit(:title, :description, :price, :quantity, :email_address)
+      params.require(:order).permit(:title, :description, :price, :quantity, :email_address, id: [])
+    end
+    
+    def get_order_with_items
+      order_id = params[:id]
+      order = Order.find(order_id)
+      order_items = order.order_items
+    
+      render json: { order: order, order_items: order_items }
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_14_150155) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_24_193823) do
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -20,6 +20,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_14_150155) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer "order_id", null: false
+    t.string "title"
+    t.string "description"
+    t.decimal "price"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -46,4 +57,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_14_150155) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "order_items", "orders"
 end
